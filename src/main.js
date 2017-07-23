42c060bd62bf026eb7b914230bea608430191986
rightusername <dristk@gmail.com>
2017-07-23 15:00:52 +0300

    dsd

--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@ import React from "react"
 import ReactDOM from "react-dom"
 import ImageGallery from "./ImageGallery.jsx"
 
-ReactDOM.render(<ImageGallery />,document.getElementById("app"));
+ReactDOM.render(<ImageGallery />,document.getElementById("app")); 
